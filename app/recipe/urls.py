fa45f44bd5b476e316f9cf24fd4f415357933770
Blaine Kruizenga <blaine@kruizer.com>
2019-12-16 10:07:30 -0600

    Add list ingredients feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -7,6 +7,7 @@ from recipe import views
 # Expected route: /api/recipe/tags/1/
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
